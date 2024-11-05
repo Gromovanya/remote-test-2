2e95b90588aff0f205b5f7f1adfbac7dd29f09b1
Gromovan <119213538+Gromovanya@users.noreply.github.com>
2024-11-05 15:07:33 +0200

    Update python.py: add func

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
 print('Hello world!')
+
+a = int(input('Введите число'))
+
+def func(a):
+  if a > 100:
+    print('Огромное число!')
+  elif a >= 10:
+    print('Среднее число!')
+  else:
+    print('Небольшое число!')
